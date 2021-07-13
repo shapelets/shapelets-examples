307e085e63b10394b15d6bdda71431cba03bb2ba
Jose Sanchez Aranda <jose.sanchez@shapelets.io>
2021-07-13 12:41:42 +0200

    Add linechart_with_views example

---
+++ dataapps/Charts/07_linechart_with_views.py
@@ -0,0 +1,44 @@
+# Copyright (c) 2021 Grumpy Cat Software S.L.
+#
+# This Source Code is licensed under the MIT 2.0 license.
+# the terms can be found in LICENSE.md at the root of
+# this project, or at http://mozilla.org/MPL/2.0/.
+
+from shapelets import init_session
+from shapelets.dsl.data_app import DataApp
+from shapelets.model import View
+
+from dataapps.util import get_enernoc_sequences, upload_enernoc_dataset
+
+# This example only works under v0.3.9 and above
+# Start shapelets process and init session as admin
+client = init_session("admin", "admin")
+
+# Create a dataApp
+app = DataApp(
+    name="07_linechart_with_views",
+    description="07_linechart_with_views"
+)
+
+# These lines below ensure that EnerNOC dataset is in shapelets platform
+upload_enernoc_dataset(client)
+enernoc_sequences = get_enernoc_sequences(client)
+sequence = enernoc_sequences[0]
+
+# Creating views
+views = list()
+offset = 100000000
+size = 1000000000
+for i in range(3):
+    begin = sequence.axis.starts + size * i + 10000000000
+    end = begin + size
+    view = View(sequence, begin, end - offset)
+    views.append(view)
+
+# Create a line_chart rendering a sequence
+line_chart = app.line_chart(title=sequence.name, sequence=sequence, views=views)
+# Place line_chart into the Dataapp
+app.place(line_chart)
+
+# Register the Dataapp
+client.register_data_app(app)
