b29b675d6c1ba14a5469a53a60d5cb09df48e4bb
Adrian Carrio <adrian.carrio@shapelets.io>
2021-07-19 13:10:14 +0100

    Removed custom function from example

--- dataapps/Use-cases/02_arrythmia_detection.py
+++ dataapps/Use-cases/02_arrythmia_detection.py
@@ -5,6 +5,9 @@
 # this project, or at http://mozilla.org/MPL/2.0/.
 from shapelets import init_session
 from shapelets.dsl.data_app import DataApp
+from shapelets import Shapelets
+from shapelets.model import Collection
+import time
 from shapelets.model import Sequence
 from shapelets.dsl import dsl_op as dsl
 from shapelets.model.view_match import View
@@ -15,55 +18,58 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
-def createSequences(csv_content:str)->str: #->typing.Tuple[Sequence, Sequence]
-    df = pd.read_csv(csv_content, header=None, skiprows=[0, 1], index_col=0, names=['MLII', 'V1'])
+def upload_sequences(client: Shapelets, df: pd.DataFrame, collection: Collection):
+    already_loaded = [sequence.name for sequence in client.get_collection_sequences(collection)]
+    loaded = 0
+    begin = time.time()
+    all_begin = time.time()
+    for column in df.columns:
+        if column not in already_loaded:
+            begin = time.time()
+            client.create_sequence(dataframe=df.loc[:, column].to_frame(), name=column, collection=collection)
+            loaded += 1
+    print(f"Loaded[{loaded}]:\t{column}\t{time.time() - begin}")
+    print(f"Total elapsed: {time.time() - all_begin}​​​​​​​")
 
-    #Convert to Sequences
-    s1 = kv.Array.from_numpy(df['MLII'].to_numpy(), khiva_type=kv.array.dtype.f64)
-    s1_column_info_entry = ColumnProtoEntry(sourceName='MLII', columnProtoEntryDataType=DT_NUMERICAL)
-    s1_column_info = ColumnProto(columnDimensions=UNIDIMENSIONAL, columnProtoEntries=[s1_column_info_entry])
-    s2 = kv.Array.from_numpy(df['V1'].to_numpy(), khiva_type=kv.array.dtype.f64)
-    s2_column_info_entry = ColumnProtoEntry(sourceName='V1', columnProtoEntryDataType=DT_NUMERICAL)
-    s2_column_info = ColumnProto(columnDimensions=UNIDIMENSIONAL, columnProtoEntries=[s2_column_info_entry])
-    sequence_axis = kv.Array.from_numpy(df.index.astype('uint64').to_numpy(), khiva_type=kv.array.dtype.u64)
-    starts0 = int(df.index[0].timestamp()*1000)
-    starts1 = int(df.index[1].timestamp()*1000)
-    every = (starts1-starts0)
-    sequence_axis_info = AxisProto(axisName="axis",
-                                   densityType=DENSE_REGULAR,
-                                   axisType=ORDINAL_AXIS,
-                                   starts=starts0,
-                                   every=every)
-    seq1 = ShapeletsSequence(sequence_axis,[s1],s1_column_info_entry.sourceName,
-                               NUMERIC,sequence_axis_info,s1_column_info,"")
-    seq2 = ShapeletsSequence(sequence_axis,[s2],s2_column_info_entry.sourceName,
-                               NUMERIC,sequence_axis_info,s2_column_info,"")
 
-    return seq1, seq2
+def get_collection(client: Shapelets, collection_name: str, collection_description: str = "No description available") -> Collection:
+    collections = client.get_collections()
+    if collection_name not in [collection.name for collection in collections]:
+        client.create_collection(name=collection_name,description=collection_description)
+    collections = client.get_collections()
+    return next(col for col in collections if col.name == collection_name)
 
 client = init_session("admin","admin")
 app = DataApp(name="02_arrythmia_detection",
 description="In this app, data from the MIT-BIH Arrhythmia Database (mitdb) are retrieved.")
 
-html_doc = requests.get('https://archive.physionet.org/cgi-bin/atm/ATM?tool=samples_to_csv&database=mitdb&rbase=102')
+html_doc = requests.get('https://archive.physionet.org/cgi-bin/atm/ATM?tool=samples_to_csv&database=mitdb&rbase=103')
 soup = BeautifulSoup(html_doc.content, 'html.parser')
 section = soup.find(id='page').find_all('pre')
-csv_content = io.StringIO(section[1].text)
+csv_content = section[1].text
 
-# Register custom function
-client.register_custom_function(createSequences)
+df = pd.read_csv(io.StringIO(csv_content), header=None, skiprows=[0, 1], index_col=0, names=['MLII', 'V1'], nrows=10)
+df.index = pd.to_datetime(df.index, unit='s')
 
-seq1, seq2 = createSequences(csv_content)
+collection = get_collection(client, collection_name = "Arrythmia dataframe collection",
+                             collection_description="This is a collection including a ECG data")
+
+# Upload the dataframe into the collection
+upload_sequences(client, df, collection)
+
+# Get the first column in the dataframe as a sequence
+seq0 = client.get_collection_sequences(collection)[0]
+#seq1 = client.get_collection_sequences(collection)[1]
 
 # Create temporal context
 tc = app.temporal_context("Temporal context")
 
 # Show companies' names and plot price data
-line_chart1 = app.line_chart(title='MLII', sequence=seq1, temporal_context=tc)
+line_chart1 = app.line_chart(title='MLII', sequence=seq0, temporal_context=tc)
 app.place(line_chart1)
 
-line_chart2 = app.line_chart(title='V1', sequence=seq2, temporal_context=tc)
-app.place(line_chart2)
+#line_chart2 = app.line_chart(title='V1', sequence=seq1, temporal_context=tc)
+#app.place(line_chart2)
 
 # Register DataApp
 client.register_data_app(app)
