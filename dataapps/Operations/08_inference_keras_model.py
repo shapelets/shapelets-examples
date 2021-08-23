8868a279a638f8ab61357d09cc87efaa9c1d4b7a
adrian-carrio <adrian.carrio@shapelets.io>
2021-08-23 13:45:31 +0200

    Adding example to run inference of Keras model

--- dataapps/Operations/08_inference_keras_model.py
+++ dataapps/Operations/08_inference_keras_model.py
@@ -9,10 +9,9 @@ from shapelets.dsl.data_app import DataApp, NDArray
 from shapelets.dsl import dsl_op
 import numpy as np
 
-def runKerasInference(input:NDArray)->typing.Tuple[float, float]:
+def runKerasInference(train_x:NDArray, train_y:NDArray, input:NDArray)->typing.Tuple[float, float]:
     from keras import Sequential
     from keras.layers import Dense
-    import numpy as np
     from numpy.random import seed
     from tensorflow import random
 
@@ -29,20 +28,16 @@ def runKerasInference(input:NDArray)->typing.Tuple[float, float]:
                   optimizer='adam',
                   metrics=['accuracy'])
 
-    # Create some training data and labels
-    train_x = np.asarray([[0, 0, 0], [0, 1, 1], [1, 0, 0], [1, 1, 1]])
-    train_y = np.asarray([0, 1, 1, 0])
-
     # Train the model
-    model.fit(train_x, train_y)
+    model.fit(train_x.values, train_y.values)
 
     # Run prediction on input values
     preds = model.predict(input.values)
 
     return preds[0][0], preds[0][1]
 
-def buildStringWithOutput(input1:float, input2:float)->str:
-    return 'Outputs are: '+str(input1) + ' and ' + str(input2)
+def buildStringWithOutput(result1:float, result2:float)->str:
+    return '# Outputs are: **'+str(result1) + '** and **' + str(result2)+'**'
 
 # Start shapelets process and init session as admin
 client = init_session("admin", "admin")
@@ -57,26 +52,28 @@ app.place(app.markdown("""
   # This Dataapp creates a Keras model and performs inference of a given input
 """))
 
-client.register_custom_function(runKerasInference)
-client.register_custom_function(buildStringWithOutput)
+# Register two functions, one to run the inference and another one to format the output into a text string
+client.register_custom_function(runKerasInference,persist_results=False)
+client.register_custom_function(buildStringWithOutput,persist_results=False)
 
-# Create input as np array and convert it to NDArray
-input = client.create_nd_array(np.asarray([[0.5, 0.8, 0.9]]))
+# Create some training data, labels and an input array and convert them to NDArrays
+train_x = client.create_nd_array(np.asarray([[0, 0, 0], [0, 1, 1], [1, 0, 0], [1, 1, 1]]))
+train_y = client.create_nd_array(np.asarray([0, 1, 1, 0]))
+input = client.create_nd_array(np.asarray([[0.5, 0.7, 0.9]]))
 
 # Run inference and get two outputs from model
-output1, output2 = dsl_op.runTensorflowInference(input)
+output1, output2 = dsl_op.runKerasInference(train_x, train_y, input)
 
 # Convert the output into a displayable string
-output_string = dsl_op.buildStringWithOutput(output1,output2)
+output_string = dsl_op.buildStringWithOutput(output1, output2)
 
 # Create button
 button = app.button(text="Make inference")
-button.on_click([output1, output2, output_string])
+button.on_click([output_string])
 app.place(button)
 
-# Create label to show results
-label = app.label(output_string)
-app.place(label)
+# Create markdown to show results
+app.place(app.markdown(output_string))
 
 # Register the Dataapp
 client.register_data_app(app)
